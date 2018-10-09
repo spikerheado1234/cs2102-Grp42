43fffbed1505d731935d5ab52f12b010257d6268
Harsh Gadodia <harsh@u.nus.edu>
2018-10-10 01:25:22 +0800

    save state on log in page

--- sharktanks-backend/server/seeders/V1-demo-user.js
+++ sharktanks-backend/server/seeders/V1-demo-user.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     queryInterface.sequelize.query("insert into users values('testing@testing.com',\
       'testuser','tester1','testrole',crypt('qqqqqq', gen_salt('md5')))");
-  },
+  }, //just add mroe queries in this format to populate db
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Users', null, {});
