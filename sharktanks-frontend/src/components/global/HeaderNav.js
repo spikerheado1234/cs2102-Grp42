4cc28e4bac89c328c6744f0c280f13ae280853ba
jeffypie369 <jffersonzzz@hotmail.com>
2018-10-12 01:36:44 +0800

    Edit query

--- sharktanks-frontend/src/components/global/HeaderNav.js
+++ sharktanks-frontend/src/components/global/HeaderNav.js
@@ -5,13 +5,13 @@ const HeaderNav = () => (
   <div className="nav-container">
     <nav className="primary-nav light">
       <Link to="/products">Projects</Link>
-      <Link to="/styles">Styles</Link>
+      <Link to="/styles">Categories</Link>
     </nav>
     <div className="logo light">
-      <Link to="/" className="logo-link">
+      <Link to="/" className="logo-link" style={{ textDecoration: 'none' }}>
         <div className="header-content">
         <span className="content">
-          <font size="12" text-align="center">SHARKSTANK</font>
+          <font size="12" text-align="center">SHARKTANKS</font>
         </span>
         </div>
       </Link>
