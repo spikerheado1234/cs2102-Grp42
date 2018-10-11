ce3a2bfa8bfed6fa7cec549f88383fcc93f171dd
spikerheado1234 <ahangupta.96@gmail.com>
2018-10-12 01:55:11 +0800

    Fix query

--- sharktanks-backend/controllers/index-controller.js
+++ sharktanks-backend/controllers/index-controller.js
@@ -163,12 +163,13 @@ exports.searchByUser = function(userId, role) {
 
 // Gives all the projects in the DB.
 exports.searchAllProjects = function() {
-	return db.sequelize.query("SELECT p1.description, p1.title, k1.words, u1.name, c1.name, p1.projectID, SUM(*)" +
+	return db.sequelize.query("SELECT p1.description, p1.title, k1.words, u1.name, c1.name, p1.projectID, SUM(d1.donationid)" +
 							   " FROM project p1, keywords k1, categories c1, users u1, donations d1" +
 							   " WHERE p1.projectID = k1.projectID AND" + 
 									  " p1.categoryID = c1.categoryID AND" +
 									  " p1.userID = u1.userID AND" +
 									  " u1.role = 'Entrepreneur'" +
+									  " d1.projectID = p1.projectID" + 
 							   " GROUP BY d1.projectID", 
 							   {type: db.sequelize.QueryTypes.SELECT})
 						.then((data) => {
